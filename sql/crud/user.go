ddcc6d7df53749a00db2ad87093db0bf0aed4167
张旭 <10939695+xv-z@user.noreply.gitee.com>
2022-05-25 13:08:28 +0000

    update sql/crud/user.go.

--- sql/crud/user.go
+++ sql/crud/user.go
@@ -1,6 +1,8 @@
 package crud
 
 import (
+	"fmt"
+
 	"gitee.com/Whitroom/imitate-tiktok/sql/models"
 	"gorm.io/gorm"
 )
@@ -10,28 +12,46 @@ func CreateUser(db *gorm.DB, user *models.User) *models.User {
 	return user
 }
 
-func GetUser(db *gorm.DB, id uint64) *models.User {
+func GetUserByID(db *gorm.DB, userID uint) (*models.User, error) {
 	var user *models.User
-	db.First(&user, id)
-	return user
+	db.First(&user, userID)
+	if user == nil {
+		return nil, fmt.Errorf("未找到用户")
+	}
+	return user, nil
 }
 
-func GetUserByName(db *gorm.DB, name string) *models.User {
-	var user *models.User
-	db.Where(&models.User{Name: name}).First(&user)
-	return user
+func GetUsersByName(db *gorm.DB, name string) []models.User {
+	var users []models.User
+	db.Where(&models.User{Name: name}).First(&users)
+	return users
 }
 
-func GetUserSubscribersByName(db *gorm.DB, name string) *models.User {
-	var user *models.User
-	db.Where(&models.User{Name: name}).Preload("Subscribers").Find(&user)
-	return user
+func SubscribeUser(db *gorm.DB, userID uint, subscriberUserID uint) (*models.User, error) {
+	var subscriber, user *models.User
+	db.First(&subscriber, subscriberUserID)
+	db.First(&user, userID)
+	if subscriber == nil {
+		return nil, fmt.Errorf("未找到关注人")
+	}
+	if user == nil {
+		return nil, fmt.Errorf("未找到用户")
+	}
+	db.Model(&user).Association("Subscriber").Append(&subscriber)
+	return user, nil
 }
 
-func GetUserFollowersByName(db *gorm.DB, name string) *models.User {
-	user := GetUserByName(db, name)
+func GetUserSubscribersByID(db *gorm.DB, userID uint) []models.User {
+	var user *models.User
+	db.Preload("Subscribers").Find(&user, userID)
+	return user.Subscribers
+}
+
+func GetUserFollowersByName(db *gorm.DB, userID uint) []models.User {
+	var followers []models.User
 	db.Raw("select * from users where id in"+
-		"(select user_id from subscribes, `users`"+
-		"where `users`.id = subscriber_id and subscriber_id = ?)", user.ID).Scan(&user.Followers)
-	return user
+		"(select user_id from subscribes left join `users`"+
+		"on `users`.id = subscriber_id "+
+		"where subscriber_id = ?)", userID).Scan(&followers)
+	return followers
 }
